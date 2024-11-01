0cd79f89274eb8617cdefb0cbadad521e7104137
Kaif <129430112+itskaif07@users.noreply.github.com>
2024-11-02 00:37:06 +0530

    categoryDelete

--- ECommerceApp/Controllers/CategoriesController.cs
+++ ECommerceApp/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using ECommerceApp.Models;
 using Microsoft.AspNetCore.Identity.UI.V4.Pages.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceApp.Controllers
 {
@@ -59,6 +60,47 @@ namespace ECommerceApp.Controllers
             }
 
             return View(category); 
+        }
+
+        public async Task<IActionResult> DeleteCategoryList()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        public async Task<IActionResult> CategoryDelete(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmCategoryDelete(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.Where(p => p.CategoryId == id).ToListAsync();
+
+            if (products.Any())
+            {
+                _context.Products.RemoveRange(products);
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("DeleteCategoryList");
         }
 
 
--- ECommerceApp/Controllers/ProductsController.cs
+++ ECommerceApp/Controllers/ProductsController.cs
@@ -110,6 +110,5 @@ namespace ECommerceApp.Controllers
         }
 
 
-
     }
 }
