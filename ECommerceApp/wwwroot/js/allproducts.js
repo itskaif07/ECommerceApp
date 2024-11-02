a99c1323b8614225007bdad26bb466ad8d4b3020
Kaif <129430112+itskaif07@users.noreply.github.com>
2024-11-02 20:08:20 +0530

    search

---
+++ ECommerceApp/wwwroot/js/allproducts.js
@@ -0,0 +1,19 @@
+﻿const SearchInput = document.querySelector("#SearchInput");
+const ProductContainer = document.querySelector("#ProductContainer");
+const products = ProductContainer.querySelectorAll("a"); 
+
+// Search function to filter products
+function search() {
+    const searchValue = SearchInput.value.toLowerCase(); // Get input value
+    products.forEach(product => {
+        const productName = product.getAttribute("data-product-name"); // Get the product name from data attribute
+        if (productName.includes(searchValue)) {
+            product.style.display = ""; // Show product
+        } else {
+            product.style.display = "none"; // Hide product
+        }
+    });
+}
+
+
+SearchInput.addEventListener("input", search);
